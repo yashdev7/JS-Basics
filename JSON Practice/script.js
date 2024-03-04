0e449615894b2452953bb65b403385dc9a60a6d1
yash jain <yash@mediaamp.co.in>
2024-03-04 12:30:40 +0530

    Create different Folders

---
+++ JSON Practice/script.js
@@ -0,0 +1,27 @@
+const myJson = `{
+    
+        "name": "Yash",
+        "score": 11.75,
+        "isAdmin": false,
+        "license": null,
+        "shopItem": ["food","clothes","sunflower oil"],
+    
+        "myObj": {
+            "name": "Rock",
+            "marks": 45,
+            "shopItem2": ["Apple", "Orange",{"a": true}]
+        }
+    
+}`;
+
+const parsed = JSON.parse(myJson);
+
+console.log(parsed);
+
+console.log(parsed["shopItem"]);
+
+console.log(parsed["shopItem2"]);  // this will show undefined
+console.log(parsed["myObj"]["shopItem2"]); // this will give the data inside it
+
+console.log(parsed["myObj"]["shopItem2"]["a"]); // this will show undefined
+console.log(parsed["myObj"]["shopItem2"][2]["a"]); // this will give the value of a is true
